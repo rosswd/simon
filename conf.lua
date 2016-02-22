03b6a4e3bd52e1b5c89737b89a23c193d87da4c0
Ross Ward <micward2@gmail.com>
2016-02-22 20:59:58 +0000

    Improve indentation

--- conf.lua
+++ conf.lua
@@ -2,8 +2,8 @@ function love.conf(t)
     -- set window title
     t.window.title = 'Simon - the game'
     
-	-- to display live console output in sublime text 2
-	io.stdout:setvbuf("no")
+    -- to display live console output in sublime text 2
+    io.stdout:setvbuf("no")
 
     -- windows debugging
     t.console = true
